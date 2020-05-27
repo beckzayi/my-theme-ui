b6ffdb6a13a63505b875e509fd40a80e111a0047
Darren Li <beckzayi@126.com>
2020-05-27 16:04:14 +1000

    header, footer

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+  <footer>
+    <div>
+      <a href="https://www.facebook.com/enableHRAU/" target="_blank" rel="noopener noreferrer">
+        <img src="/images/facebook.svg" alt="Facebook" />
+      </a>
+      <a href="https://www.linkedin.com/company/enablehr" target="_blank" rel="noopener noreferrer">
+        <img src="/images/linkedin.svg" alt="Linkedin" />
+      </a>
+      <a href="https://twitter.com/enableHR" target="_blank" rel="noopener noreferrer">
+        <img src="/images/twitter.svg" alt="Twitter" />
+      </a>
+    </div>
+  </footer>
+);
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <header>
+    <Link to="/">
+      <img src="/images/logo.svg" title="enableHR" alt="enableHR" />
+    </Link>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/docs">Docs</Link></li>
+    </ul>
+  </header>
+);

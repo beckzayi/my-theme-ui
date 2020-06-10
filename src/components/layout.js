67c80677da0157ce5b9daa6dfbeda7d3405581df
Darren Li <beckzayi@126.com>
2020-06-10 23:17:31 +1000

    init using theme-ui sx and components in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,51 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+import { Flex, Box } from '@theme-ui/components'
 import Head from './head'
 import Header from './header'
 import Footer from './footer'
 
 export default (props) => {
   return (
-    <div>
+    <Styled.root>
       <Head {...props} />
-      <Header />
-      <main>{props.children}</main>
-      <Footer />
-    </div>
+      <Flex
+        sx={{
+          flexDirection: 'column',
+          minHeight: '100vh',
+        }}>
+        <Flex
+          as="header"
+          sx={{
+            height: 64,
+            px: 3,
+          }}>
+          <Header />
+        </Flex>
+        <Box
+          sx={{
+            flex: '1 1 auto',
+          }}>
+          <div
+            sx={{
+              display: ['block', 'flex'],
+            }}>
+            <main
+              id="content"
+              sx={{
+                width: '100%',
+                minWidth: 0,
+                maxWidth: 768,
+                mx: 'auto',
+                px: 3,
+              }}>
+              {props.children}
+            </main>
+            <Footer />
+          </div>
+        </Box>
+        >
+      </Flex>
+    </Styled.root>
   )
 }

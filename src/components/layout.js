8e00cd94c3c58ab758ee4dffe2bd3ab76966abc7
Darren Li <beckzayi@126.com>
2020-06-23 01:47:21 +1000

    footer (theme-ui footer a3)

--- src/components/layout.js
+++ src/components/layout.js
@@ -79,6 +79,26 @@ export default (props) => {
             </main>
           </Flex>
         </Flex>
+        <footer
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'center',
+            p: 2,
+            variant: 'styles.footer',
+          }}>
+          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>
+            Home
+          </Link>
+          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>
+            Docs
+          </Link>
+          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>
+            Development
+          </Link>
+          <div sx={{ p: 2 }}>© 2020 enableHR</div>
+        </footer>
       </Box>
     </Styled.root>
   )

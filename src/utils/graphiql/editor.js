dc28e04f1aa6f279374151a77f9d22fcf97c6ccb
Darren Li <beckzayi@126.com>
2020-06-26 02:30:19 +1000

    graphiql: add graphiql.css if it unmet peer dependencies

--- src/utils/graphiql/editor.js
+++ src/utils/graphiql/editor.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import GraphiQL from 'graphiql'
 import graphQLFetcher from './fetcher'
+import 'graphiql/graphiql.css'
 
 export default (props) => (
   <div style={{ height: '70vh' }}>

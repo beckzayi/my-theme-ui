40fa5edf9f19feb03906f2ac53c1b299282d5abe
Darren Li <beckzayi@126.com>
2020-06-30 00:51:51 +1000

    feat:metadata, <title> in <head>

--- src/components/head.js
+++ src/components/head.js
@@ -4,13 +4,12 @@ import { Helmet } from 'react-helmet'
 import useSiteMetadata from '../hooks/use-site-metadata'
 
 export default (props) => {
-  const { description } = useSiteMetadata()
-
+  const { description, title: siteTitle } = useSiteMetadata()
   const title = [
     props.title,
     props.pageContext.frontmatter ? props.pageContext.frontmatter.title : false,
     props._frontmatter ? props._frontmatter.title : false,
-    'Theme UI',
+    siteTitle,
   ]
     .filter(Boolean)
     .join(' – ')

07b3ef85edcfa9c38d83efe1ff7e9026fec6fb07
Darren Li <beckzayi@126.com>
2020-06-30 04:31:12 +1000

    chore: pages in getting started, update content

--- src/utils/graphiql/link.js
+++ src/utils/graphiql/link.js
@@ -4,13 +4,18 @@ import { Link } from 'gatsby'
 
 const endpoint_graphql = process.env.GATSBY_ENDPOINT_GRAPHQL
 
-export default ({ query, text = 'Start Now', target = true }) => (
+export default ({
+  query,
+  text = 'Start Now',
+  target = true,
+  button = true,
+}) => (
   <Link
     to={`${endpoint_graphql}?query=${encodeURI(query)}`}
     target={target ? '_blank' : '_self'}
     rel="noreferrer"
     sx={{
-      variant: 'links.button',
+      variant: button ? 'links.button' : null,
     }}>
     {text}
   </Link>
